beaf2dc2f3c74d849b7a82fe72807b8d6f984bbb
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-11-12 00:53:13 -0300

    Configurando data e hora. Adicionando events nos botões. Implementação inicial das operações.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,26 +1,162 @@
 class CalcController{
 
     constructor(){
+        this._operation = []
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector("#display")
+        this._dateEl = document.querySelector("#data")
+        this._timeEl = document.querySelector("#hora")
+        this._currentDate
+        this.initialize()
+        this.initButtonEvents()
+    }
 
-        this._displayCalc = "0"
-        this._dataAtual
+    initialize(){
+        
+        this.setDisplayDateTime()
+        
+        setInterval(()=>{
+            this.setDisplayDateTime()
+        }, 1000)
+    }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event=>{
+            element.addEventListener(event, fn, false)
+        })
+    }
+
+    clearAll(){
+        this._operation = []
+    }
+
+    clearEntry(){
+
+        this._operation.pop()
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value)
+
+    }
+
+    setError(){
+        this.displayCalc = 'Error'
+    }
+
+    execBtn(value){
+
+        switch(value){
+
+            case 'ac':
+                this.clearAll()
+                break
+            case 'ce':
+                this.clearEntry()
+                break
+            case 'soma':
+                
+                break
+            case 'subtracao':
+                
+                break
+            case 'divisao':
+                
+                break
+            case 'multiplicacao':
+                
+                break
+            case 'porcento':
+                
+                break
+            case 'igual':
+                
+                break
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation( parseInt(value))
+            default:
+                this.setError()
+                break
+        }
+
+    }
+
+    initButtonEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+        buttons.forEach((btn,index)=>{
+            
+            this.addEventListenerAll(btn, 'click drag', e=>{
+
+                let textBtn = btn.className.baseVal.replace("btn-", "")
+
+                this.execBtn(textBtn);
+
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
+
+                btn.style.cursor = "pointer"
+
+            })
+
+        })
+
+    }
+
+
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day:"2-digit",
+            month: "long",
+            year: "numeric"
+        })
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value
     }
 
     get displayCalc(){
-        return this._displayCalc
+        return this._displayCalcEl.innerHTML
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value
     }
 
-    get dataAtual(){
-        return this._dataAtual
+    get currentDate(){
+        return new Date()
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor
+    set currentDate(value){
+        this._currentDate = value
     }
 
 }

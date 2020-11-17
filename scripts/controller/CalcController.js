2ef536cfe237f56fa816fdf47b950fadf4b1f9de
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-11-17 16:30:27 -0300

    Incluindo sons ao clicar nos botões, tratando alguns problemas no tamanho maximo do numero no display e tratamento de exeções no eval()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -196,8 +196,20 @@ class CalcController{
 
     getResult(){
 
-        return eval(this._operation.join(""))
+        try{
 
+            return eval(this._operation.join(""))
+
+        }catch(e){
+
+            setTimeout(() => {
+
+                this.setError()
+
+            }, 250);
+            
+        }
+        
     }
 
     calc(){
@@ -441,6 +453,12 @@ class CalcController{
     }
 
     set displayCalc(value){
+
+        if(value.toString().length > 10){
+            this.setError()
+            return false
+        }
+
         this._displayCalcEl.innerHTML = value
     }
 

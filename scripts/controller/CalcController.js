9bf368466e5badf4c92f374f9fdf84a1a9aa9b40
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-11-17 15:27:50 -0300

    Incluindo audio ao apartar os botões.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,9 +2,10 @@ class CalcController{
 
     constructor(){
 
+        this._audio = new Audio('click.mp3')
         this._lastOperator = ''
         this._lastNumber = ''
-
+        this._audioOnOff = false
         this._operation = []
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display")
@@ -54,11 +55,41 @@ class CalcController{
         }, 1000)
 
         this.setLastNUmberToDisplay()
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio()
+
+            })
+
+        })
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff
+
+    }
+
+    playAudio(){
+
+        if(this._audioOnOff){
+
+            this._audio.currentTime = 0
+
+            this._audio.play()
+
+        }
+
     }
 
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{
+
+            this.playAudio()
 
             switch(e.key){
         
@@ -303,6 +334,8 @@ class CalcController{
     }
 
     execBtn(value){
+
+        this.playAudio()
 
         switch(value){
 
